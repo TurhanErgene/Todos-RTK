005b528a2de04979ca60b7ea1b3d6903a9b08257
Turhan <turhan-ergene@hotmail.com>
2022-10-09 03:06:35 +0200

    createAsyncThunk

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { useState } from "react";
 import "./App.css";
 import { add, remove, toggleCompleted } from "./features/todoSlice";
 import { useAppDispatch, useAppSelector } from "./store";
+import { fetchUser } from "./features/userSlice";
 
 function App() {
   const todos = useAppSelector((state) => state.todos);
   const [title, setTitle] = useState("");
+
+  const user = useAppSelector((state) => state.user);
 
   const dispatch = useAppDispatch();
 
@@ -20,8 +23,10 @@ function App() {
   };
 
   const toggle = (id: string) => {
-    dispatch(toggleCompleted(id))
+    dispatch(toggleCompleted(id));
   };
+
+  const currentUser = user.data && user.data.results[0];
 
   return (
     <div className="App">
@@ -35,12 +40,30 @@ function App() {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id as string}>
-            <button onClick={() => toggle(todo.id as string)}>{todo.completed ? "Mark not completed": "Mark completed"}</button>
+            <button onClick={() => toggle(todo.id as string)}>
+              {todo.completed ? "Mark not completed" : "Mark completed"}
+            </button>
             <button onClick={() => onDelete(todo.id as string)}>Delete</button>
             <span>{todo.title}</span>
           </li>
         ))}
       </ul>
+
+      <div>
+        <button onClick={() => dispatch(fetchUser())}>Fetch User</button>
+        {user.loading && "Loading..."}
+        {user.error && "error:" + user.error}
+        {currentUser && (
+          <div>
+            Name: {currentUser.name.title} {currentUser.name.first}{" "}
+            {currentUser.name.last}
+            <br />
+            Email: {currentUser.email}
+            <br />
+            Avatar: <img src={currentUser.picture.large} alt="Avatar" />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
